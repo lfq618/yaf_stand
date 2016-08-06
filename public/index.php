5a7d2f901079cb8422199aebf3331e31f187b7bf
lfq618 <lfq618@gmail.com>
2016-08-06 16:31:51 +0800

    f

--- public/index.php
+++ public/index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 define('APPLICATION_PATH', realpath(dirname(__FILE__).'/../'));
 defined('YAF_ENVIRON') || define('YAF_ENVIRON', 'develop');
@@ -13,19 +12,3 @@ $application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
 
 $application->bootstrap()->run();
 ?>
-=======
-<?php
-define('APPLICATION_PATH', realpath(dirname(__FILE__).'/../'));
-defined('YAF_ENVIRON') || define('YAF_ENVIRON', 'develop');
-
-
-if (! extension_loaded("yaf"))
-{
-	include APPLICATION_PATH . '/framework/loader.php';
-}
-
-$application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
-
-$application->bootstrap()->run();
-?>
->>>>>>> 2ac61bc1fb5ba72b79c5f9748f9a49746096d72e
