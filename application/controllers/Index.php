2d8fb2fd1f9cbb5485aafcd3b5d09b0cc4f660c1
lfq618 <lfq618@gmail.com>
2016-08-15 11:12:14 +0800

    ff

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,10 +1,29 @@
 <?php
 class IndexController extends Yaf_Controller_Abstract
 {
+    const CONTROL_COOK      = 4;   //是否烹饪标识位
+    const CONTROL_GARNISH   = 2;   //是否配菜标识位
+    const CONTROL_RECOMMEND = 1;   //是否推荐标识位
+    
 	public function indexAction() {
-		$obj = new Task_RedisStore('main');
-		
-		$ret = $obj->addTask('IMNOTICE', 'aaaaaa');
-		var_dump($ret);
+	   $num = intval($this->getRequest()->getQuery('num'));
+	   
+	   if ($num & self::CONTROL_COOK) {
+	       echo "需要烹饪";
+	   } else {
+	       echo "不需要烹饪";
+	   }
+	   echo "<br />";
+	   if ($num & self::CONTROL_GARNISH) {
+	       echo "需要配菜";
+	   } else {
+	       echo "不需要配菜";
+	   }
+	   echo "<br />";
+	   if ($num & self::CONTROL_RECOMMEND) {
+	       echo "推荐";
+	   } else {
+	       echo "不推荐";
+	   }
 	}
 }
