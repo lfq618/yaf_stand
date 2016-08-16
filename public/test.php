26621af923cf622b2e2cebe0c7063e15d0ff9f6a
lfq618 <lfq618@gmail.com>
2016-08-16 09:53:48 +0800

    测试

---
+++ public/test.php
@@ -0,0 +1,24 @@
+<?php
+while (1) {
+    $bWaitFlag = false;  //是否等待进程结束
+    $intNum    = 3;      //进程总数
+    $pids = array();  //进程pid数组
+    
+    for ($i = 0; $i < $intNum; $i++) {
+        $pids[$i] = pcntl_fork();   //产生子进程，而且从当前行之下开始运行代码，而且不继承父进程的数据信息
+        if ($pids[$i] == -1) {
+            echo "couldn't fork.\n";
+        } elseif (! $pids[$i]) {
+            sleep(1);
+            echo "第{$i}个进程-->" . time() . "\n";
+            exit(0);  //子进程要exit，否则会进行递归多进程，父进程不要exit，否则终止多进程.
+        }
+        
+        if ($bWaitFlag) {
+            pcntl_waitpid($pids[$i], $status, WUNTRACED);
+            echo "wait {$i} -> " . time() . "\n";
+        }
+        
+    }
+    sleep(1);
+}
