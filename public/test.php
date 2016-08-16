cdfc52ad366bbf26835b0c0a5fe4c2587e1265fe
lfq618 <lfq618@gmail.com>
2016-08-16 10:01:56 +0800

    f

--- public/test.php
+++ public/test.php
@@ -3,7 +3,7 @@ while (1) {
     $bWaitFlag = true;  //是否等待进程结束
     $intNum    = 3;     //进程总数
     $pids = array();  //进程pid数组
-    
+    echo "I am a master." . time() . "\n";
     for ($i = 0; $i < $intNum; $i++) {
         $pids[$i] = pcntl_fork();   //产生子进程，而且从当前行之下开始运行代码，而且不继承父进程的数据信息
         if ($pids[$i] == -1) {
