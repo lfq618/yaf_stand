6946d02592663f4c7a30493ccf6d07bf44156f2c
lifuqiang <lifq618@163.com>
2016-09-08 22:56:03 +0800

    f

--- public/test.php
+++ public/test.php
@@ -6,10 +6,13 @@ while (1) {
     echo "I am a master." . time() . "\n";
     for ($i = 0; $i < $intNum; $i++) {
         $pids[$i] = pcntl_fork();   //产生子进程，而且从当前行之下开始运行代码，而且不继承父进程的数据信息
+        
         if ($pids[$i] == -1) {
             echo "couldn't fork.\n";
         } elseif (! $pids[$i]) {
-            sleep(1);
+            cli_set_process_title("第{$i}个进程");
+            sleep(10);
+            
             echo "第{$i}个进程-->" . time() . "\n";
             exit(0);  //子进程要exit，否则会进行递归多进程，父进程不要exit，否则终止多进程.
         }
