0963719a1d14d92417a02e95b6b55f89a5da5fce
lifuqiang <lifq618@163.com>
2016-10-08 13:08:47 +0800

    f

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -18,11 +18,61 @@ class TestController extends Yaf_Controller_Abstract {
                         $line = fgets($handle);
                         $line = str_replace('CRIT:', '', $line);
                         $lineData = json_decode(trim($line), true);
-                        var_dump($lineData);
-                        exit;
+                        
+                        if (! isset($objectAry[$lineData['uid']])) {
+                            $objectAry[$lineData['uid']] = $lineData['name'];
+                        }
                     }
+                    fclose($handle);
                 }
+                
+                $tm += 86400;
             }
         }
+        
+        foreach ($objectAry as $uid => $name) {
+            Fn::writeLog("{$uid}\t{$name}", "/logs/noObject.log");
+        }
+        
+        echo "ok";
+    }
+    
+    public function typeAction() {
+        $objectAry = [];
+        $logAry = [
+            '/da0/logs/poi',
+            '/da0/logs/poi2',
+        ];
+    
+        foreach ($logAry as $logPath) {
+            $tm = strtotime('2016-09-30');
+            while ($tm < time()) {
+                $file = $logPath . '/noPoiType-' . date('Ymd') . '.log';
+                if (is_file($file)) {
+                    $handle = fopen($file, 'r');
+                    while (! feof($handle)) {
+                        $line = fgets($handle);
+                        $line = str_replace('CRIT:', '', $line);
+                        list($name, $code) = explode("\t", $line);
+                        echo $name;
+                        echo "<hr />";
+                        echo $code;
+                        exit;
+//                         if (! isset($objectAry[$lineData['uid']])) {
+//                             $objectAry[$lineData['uid']] = $lineData['name'];
+//                         }
+                    }
+                    fclose($handle);
+                }
+    
+                $tm += 86400;
+            }
+        }
+    
+        foreach ($objectAry as $uid => $name) {
+            Fn::writeLog("{$uid}\t{$name}", "/logs/noPoiType.log");
+        }
+    
+        echo "ok";
     }
 }
